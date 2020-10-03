2c0d2af4dbc479730af78a4161d7646462354dc2
satwik04 <56727147+satwik04@users.noreply.github.com>
2020-10-03 11:46:08 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -2,14 +2,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c,d,max1,max2;
+	int s,p,c,d,max1,max2;
 	printf("Enter the numbers:");
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	//comparing a & b
-	max1=a>=b?a:b;
+	scanf("%d %d %d %d",&s,&p,&c,&d);
+	//comparing p & s
+	max1=s>=p?s:p;
 	//comparing c & d
 	max2=c>=d?c:d;
-	//comparing the max values between (a and b) & (c and d)
+	//comparing the max values between (s and p) & (c and d)
 	max1>=max2?printf("Maximum=%d",max1):printf("Maximum:%d",max2);
     return 0;
 }
